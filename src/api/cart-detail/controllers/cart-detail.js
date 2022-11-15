3f8b5a1d37c666e341ecb8e4ddcfeb13525c2df6
vampire32 <moizabdul320@gmail.com>
2022-11-15 12:48:43 +0500

    update

--- src/api/cart-detail/controllers/cart-detail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cart-detail controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::cart-detail.cart-detail');
--- src/api/cart-detail/routes/cart-detail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cart-detail router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::cart-detail.cart-detail');
